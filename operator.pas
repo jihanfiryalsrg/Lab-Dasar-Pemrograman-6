1abab309ebb5c63fd351a2cf3093607fa91a69a5
jihanfiryalsrg <jihanfiryalsrg@gmail.com>
2025-10-10 00:33:19 +0700

    pertemuan 2 : operator

---
+++ operator.pas
@@ -0,0 +1,45 @@
+uses crt;
+
+var
+tambah, kurang, kali, bagi : integer;
+
+begin
+    clrscr;
+    // writeln('Hello world');
+    a := 10;
+    b := 3;
+
+    // tambah := a + b;
+    // kurang := a - b;
+    // kali := a * b;
+    // bagi := a / b;
+    // modulo := a mod b;
+
+    // writeln('Hasil jumlah : ', tambah);
+    // writeln('Hasil kurang : ', kurang);
+    // writeln('Hasil kali : ', kali);
+    // writeln('Hasil bagi : ', bagi:0:2); //4.00
+    // writeln('Hasil mod : ', modulo);
+
+    // writeln(a = b);
+    // writeln(a <> b);
+    // writeln(a < b);
+    // writeln(a > b);
+    // writeln(a <= b);
+    // writeln(a >= b);
+
+    // writeln(true and true);
+    // writeln(true and false);
+    // writeln(false and true);
+    // writeln(false and false);
+
+    // writeln(true or true);
+    // writeln(true or false);
+    // writeln(false or true);
+    // writeln(false or false);
+
+    // writeln(not true);
+    // writeln(not false);
+
+    
+end.
